28cca0155a930bcbafe324ef4e4ec5590f67cc27
AmirZNBZ <amir.na.sufi1376@gmail.com>
2023-10-15 21:37:56 +0330

    convert to jsx and folder

--- srcWithoutContext/components/App.js
+++ srcWithoutContext/components/App.js
@@ -1,16 +1,15 @@
 import { useEffect, useReducer } from "react";
-
-import Main from "./main";
-import Timer from "./Timer";
-import Error from "./Error";
-import Footer from "./Footer";
 import Header from "./Header";
+import Main from "./main";
 import Loader from "./Loader";
-import Question from "./Question";
-import Progress from "./Progress";
-import NextButton from "./NextButton";
+import Error from "./Error";
 import StartScreen from "./StartScreen";
+import Question from "./Question";
+import NextButton from "./NextButton";
+import Progress from "./Progress";
 import FinishScreen from "./FinishScreen";
+import Timer from "./Timer";
+import Footer from "./Footer";
 
 const SECS_PER_QUESTION = 30;
 
